3b1ae40c04e5a7ef3e0ed43bc6595d4f0b44bd78
Yuri Kiev de Sousa Barreto <yurikiev0302@gmail.com>
2025-03-13 12:12:20 -0300

    adicionando exibicao de erros no html

--- cadastro.js
+++ cadastro.js
@@ -1,9 +1,12 @@
-
 const btnEntrar = document.getElementById('enviar')
-
-
-
+const arrayErros = []
 btnEntrar.addEventListener('click', e =>{
+
+    if (!form.checkValidity()) {
+        // O navegador exibirá os erros normalmente
+        return;
+    }
+    
     e.preventDefault()
     //pegando os elementos do formulario
     const user = document.getElementById('user')
@@ -13,18 +16,22 @@ btnEntrar.addEventListener('click', e =>{
 
 
     //se passar da validacao, cria um novo objeto Usuario e manda para o localStorage
-    if(!valida(user.value, email.value, senha.value, confirmaSenha.value)){
-        console.log('deu erro na situação')
-        user.value = ''
-        email.value = ''
-        senha.value = ''
-        confirmaSenha.value = ''
+    if(valida(user.value, email.value, senha.value, confirmaSenha.value !== true)){
+        
+        for(let err of arrayErros){
+            console.log(err)
+            exibeErro(err)
+        }
+        
+       
+      
         return
     }
     else{
         const usuario = new Usuario(user.value, email.value, senha.value)
         const usuarioJson = JSON.stringify(usuario)
         localStorage.setItem(id(), usuarioJson)
+        console.log('cadastroCriado')
     }
 
    
@@ -43,14 +50,18 @@ class Usuario{
     function valida(user, email, senha, confirmaSenha){
     //valida se o nome de usuario já existe
     if(!validaNome(user, email)){
-        alert('erro na valdicao de nome')
-        return false
+        arrayErros.push('nome de Usuário e/ou email ja existe')
+        //return false
 
     }
     //valida se os campos de senha sao iguais
+    
+
     if(senha !== confirmaSenha){
-        alert('senha e confirma diferentes')
-        return false
+        arrayErros.push('senha e confirmação de senha diferentes')
+        senha.value = ''
+        confirmaSenha.value = ''
+        //return false
     }
 
     
@@ -70,8 +81,10 @@ class Usuario{
 
     //verifica se tem ao menos uma letra maiuscula, minuscula e um numero
     if(maiusculo === 0 || num === 0 || minusculo === 0){
-        alert('faltando maiscula, minusculo ou numero na senha')
-        return false
+        arrayErros.push('SENHA: faltando maiscula, minusculo ou numero')
+        senha.value = ''
+        confirmaSenha.value = ''
+        //return false
     }
 
 
@@ -88,13 +101,15 @@ function validaNome(username, userEmail){
         console.log('tamanho:' , localStorage.length)
 
         if(Obj.user === username){
-            alert('nome de usuario ja existe')
-            return false
+            arrayErros.push('nome de usuario ja existe')
+            user.value = ''
+            //return false
         } 
 
         if(Obj.email === userEmail){
-            alert('email ja existe')
-            return false
+            arrayErros.push('email ja existe')
+            email.value = ''
+           // return false
         }
     }
 
@@ -104,6 +119,20 @@ function validaNome(username, userEmail){
 //gera um novo numero para ser usado como chave no localStorage
 function id(){
  return (localStorage.length + 1)
+}
+
+
+function exibeErro(erro){
+    const divErros =  document.getElementById('erros')
+    const listaErros = document.getElementById('listaErr')
+    const li = document.createElement('li')
+    li.innerHTML = erro
+    listaErros.appendChild(li)
+    divErros.style.display = 'block'
+
+    setTimeout(() => {
+        listaErros.removeChild(li)
+    }, 18000);
 }
 
 //cria hash de senha
