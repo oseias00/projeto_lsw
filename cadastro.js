31b11e1301e28679c2867a74bf682e40c1642222
Yuri Kiev de Sousa Barreto <yurikiev0302@gmail.com>
2025-03-12 23:44:09 -0300

    criando a garantia de que não vão existir mais de um usuario com mesmo username ou email

--- cadastro.js
+++ cadastro.js
@@ -1,4 +1,7 @@
+
 const btnEntrar = document.getElementById('enviar')
+
+
 
 btnEntrar.addEventListener('click', e =>{
     e.preventDefault()
@@ -9,18 +12,19 @@ btnEntrar.addEventListener('click', e =>{
     const confirmaSenha = document.getElementById('confirmaSenha')
 
 
-    console.log(user.value, email.value, senha.value, confirmaSenha.value)
-
-    //se passar da validacao, cria um novo objeto Usuario
+    //se passar da validacao, cria um novo objeto Usuario e manda para o localStorage
     if(!valida(user.value, email.value, senha.value, confirmaSenha.value)){
-        alert('deu erro na situação')
-        
+        console.log('deu erro na situação')
+        user.value = ''
+        email.value = ''
+        senha.value = ''
+        confirmaSenha.value = ''
+        return
     }
     else{
-        alert('situacao deu certo')
-        const u1 = new Usuario(user.value, email.value, senha.value)
-        console.log(u1)
-
+        const usuario = new Usuario(user.value, email.value, senha.value)
+        const usuarioJson = JSON.stringify(usuario)
+        localStorage.setItem(id(), usuarioJson)
     }
 
    
@@ -34,11 +38,15 @@ class Usuario{
         this.email = email
         this.senha = senha
     }
+
 }
+    function valida(user, email, senha, confirmaSenha){
+    //valida se o nome de usuario já existe
+    if(!validaNome(user, email)){
+        alert('erro na valdicao de nome')
+        return false
 
-function valida(user, email, senha, confirmaSenha){
-    //valida
-
+    }
     //valida se os campos de senha sao iguais
     if(senha !== confirmaSenha){
         alert('senha e confirma diferentes')
@@ -60,7 +68,6 @@ function valida(user, email, senha, confirmaSenha){
         
     }
 
-    console.log('maiucsulo: ', maiusculo, 'num: ', num, 'minusculo:', minusculo)
     //verifica se tem ao menos uma letra maiuscula, minuscula e um numero
     if(maiusculo === 0 || num === 0 || minusculo === 0){
         alert('faltando maiscula, minusculo ou numero na senha')
@@ -68,7 +75,36 @@ function valida(user, email, senha, confirmaSenha){
     }
 
 
-    alert('tudo ok')
     return true
-    
 }
+
+//verifica se ja tem alguem com o mesmo nome de usuario ou mesmo email
+function validaNome(username, userEmail){
+    for(let i = 0; i<localStorage.length; i++){
+        console.log(i)
+        const chave = localStorage.key(i)
+        const valor = localStorage.getItem(chave)
+        const Obj = JSON.parse(valor)
+        console.log('tamanho:' , localStorage.length)
+
+        if(Obj.user === username){
+            alert('nome de usuario ja existe')
+            return false
+        } 
+
+        if(Obj.email === userEmail){
+            alert('email ja existe')
+            return false
+        }
+    }
+
+    return true
+}
+
+//gera um novo numero para ser usado como chave no localStorage
+function id(){
+ return (localStorage.length + 1)
+}
+
+//cria hash de senha
+
