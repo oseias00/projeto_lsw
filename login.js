09555e78e461890fdd38c2ef25cd9a04dd93fc1e
Yuri Kiev de Sousa Barreto <yurikiev0302@gmail.com>
2025-03-13 22:48:47 -0300

    apagando comentarios desnecessarios no codigo

--- login.js
+++ login.js
@@ -37,36 +37,6 @@ function Login(){
         exibeErro('Ainda não há nenhum usuário cadastrado') 
     }
   
-    /*for(let i = 0; i<localStorage.length; i++){
-        const objVazio = {email: 'obj vazio', senha: 'objvazio'}
-        const chave = localStorage.key(i)
-        const valor = localStorage.getItem(chave)
-
-        //garante que 'Obj' vai receber apenas Objetos(tem um item no LocalStorage que determina o usuario da sessao)
-        let Obj
-        try{
-            Obj = typeof JSON.parse(valor) === "object" ? JSON.parse(valor) : objVazio
-        }
-        catch(e){
-            Obj = objVazio
-        }
-        
-        
-        console.log(chave)
-        console.log(email.value , Obj.email)
-        console.log(senha.value , Obj.senha)
-       
-        if(email.value === Obj.email && senha.value === Obj.senha){
-            console.log('login efetuado com sucesso')
-            localStorage.setItem("Sessão", Obj.user)
-            return true
-        }
-        else{
-            console.log('--------------------------')
-        }
-    
-    }
-    return false*/
    
 }
 
